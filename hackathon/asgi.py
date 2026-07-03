8e2fa11de8ad46e8cfcfda04a25cb1d52b2c5382
Samya Aggarwal <samyaggarwal005@gmail.com>
2026-07-03 19:41:54 +0530

    Fix ASGI initialization

--- hackathon/asgi.py
+++ hackathon/asgi.py
@@ -1,16 +1,22 @@
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hackathon.settings")
+
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackathon.settings')
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
+        ),
+    }
+)
